cde23b03adaaa89c23fc1954a6344bb81c12223e
Bassant Sayed <85302147+bassanttsayed@users.noreply.github.com>
2022-12-10 23:47:30 +0200

    Update perception.py

--- perception.py
+++ perception.py
@@ -131,9 +131,17 @@ def perception_step(Rover):
     
     # 6) Convert rover-centric pixel values to world coordinates
     # 7) Update Rover worldmap (to be displayed on right side of screen)
+    obstacle_x_world, obstacle_y_world = pix_to_world(oxpix,oypix,Rover.pos[0],Rover.pos[1],Rover.yaw,worldmap.shape[0],scale)
+    rock_x_world, rock_y_world = pix_to_world(rxpix,rypix,Rover.pos[0],Rover.pos[1],Rover.yaw,worldmap.shape[0],scale)
+    navigable_x_world, navigable_y_world = pix_to_world(xpix,ypix,Rover.pos[0],Rover.pos[1],Rover.yaw,worldmap.shape[0],scale)
         # Example: Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
         #          Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
         #          Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
+    if ((Rover.pitch < 1 or Rover.pitch > 359) and (Rover.roll < 1 or Rover.roll > 359)):
+    Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] = 200
+    Rover.worldmap[navigable_y_world, navigable_x_world, 0] = 0
+    Rover.worldmap[rock_y_world, rock_x_world, 1] = 200
+    Rover.worldmap[navigable_y_world, navigable_x_world, 2] = 200
 
     # 8) Convert rover-centric pixel positions to polar coordinates
     # Update Rover pixel distances and angles
